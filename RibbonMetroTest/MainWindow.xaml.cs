a2e5d6a5c5b8f93798878eb76eca2c451f587671
Bendik Engebretsen <bendik.engebretsen@pixedit.com>
2018-09-12 11:19:11 +0200

    Add project files.

---
+++ RibbonMetroTest/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using Fluent;
+using MahApps.Metro.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RibbonMetroTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : IRibbonWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MetroWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.TitleBar = this.FindChild<RibbonTitleBar>("RibbonTitleBar");
+            this.TitleBar.InvalidateArrange();
+            this.TitleBar.UpdateLayout();
+        }
+
+    #region TitleBar
+
+        /// <summary>
+        /// Gets ribbon titlebar
+        /// </summary>
+        public RibbonTitleBar TitleBar
+        {
+            get { return (RibbonTitleBar)this.GetValue(TitleBarProperty); }
+            private set { this.SetValue(TitleBarPropertyKey, value); }
+        }
+
+        // ReSharper disable once InconsistentNaming
+        private static readonly DependencyPropertyKey TitleBarPropertyKey = DependencyProperty.RegisterReadOnly(nameof(TitleBar), typeof(RibbonTitleBar), typeof(MainWindow), new PropertyMetadata());
+
+        /// <summary>
+        /// <see cref="DependencyProperty"/> for <see cref="TitleBar"/>.
+        /// </summary>
+        public static readonly DependencyProperty TitleBarProperty = TitleBarPropertyKey.DependencyProperty;
+
+    #endregion
+
+    }
+}
